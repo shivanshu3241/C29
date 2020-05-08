d66e7863a3073018c4453e8e3a21d67227b3b7e7
shivanshu3241 <64346010+shivanshu3241@users.noreply.github.com>
2020-05-08 12:14:34 +0530

    Add files via upload

---
+++ Fruit.js
@@ -0,0 +1,33 @@
+class Fruit 
+{
+  constructor(x, y, width, height) 
+  {
+    var options = 
+    {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("apple.PNG");
+    
+    World.add(world, this.body);
+  }
+  display()
+  {
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    strokeWeight(4);
+    stroke("brown");
+    rect(0, 0, this.width, this.height);
+    image(this.image,0,0,this.width,this.height);
+    pop();
+  }
+};
---
+++ Log.js
@@ -0,0 +1,23 @@
+class Log
+{
+    constructor(x,y,width,height) 
+    {
+      var options = 
+      {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("wood.png");
+      World.add(world, this.body);
+    }
+    display()
+    {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+      image(this.image,this.width,this.height);
+    }
+  };
